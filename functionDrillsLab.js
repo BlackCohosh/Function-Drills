1216e8f246958f2c0200cba89b42c7002c78c115
Agriscapes <62765843+Agriscapes@users.noreply.github.com>
2021-10-18 15:11:30 -0500

    Added solution for Problem 9

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -143,8 +143,18 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 */
 
 //CODE HERE
-
-
+// function bigOrSmall(arr){
+//   var answers = [];
+//   for (let i =0; i < arr.length; i++) {
+//     if (arr[i] > 100) {
+//       answers.push('big')
+//     } else if (arr[i] < 100) {
+//       answers.push('small')
+//     }
+//   } arrayEvaluator = answers
+// } 
+// bigOrSmall(bigOrSmallArray)
+// console.log(arrayEvaluator)
 ////////////////// PROBLEM 10 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
 let loser = 'Glimmer'
