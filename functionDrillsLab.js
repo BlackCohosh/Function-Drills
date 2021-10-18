52599ece55b53f472296bb868879b2264474602a
Agriscapes <62765843+Agriscapes@users.noreply.github.com>
2021-10-18 15:22:29 -0500

    Added solution for Problem 10

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -157,14 +157,22 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 // console.log(arrayEvaluator)
 ////////////////// PROBLEM 10 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
-let loser = 'Glimmer'
+let loser = 'Katniss'
 /*
   Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
   The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
 */
 
 //CODE HERE
+function theEliminator(contestants, loser) {
+  for (i = 0; i < contestants.length; i++) {
+    if (contestants[i] === loser) {
+      contestants.splice(i, 1)
+    } 
+  } console.log(contestants)
+} 
 
+return theEliminator(contestants, loser)
 
 ////////////////// PROBLEM 11 ////////////////////
 let sampleString = "Hi, my name is Kylo."
